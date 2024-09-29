fdc372b7d9c093cbc69a8651f4b54bfe7b14f913
shogo.takasaki <stakasaki707@gmail.com>
2024-09-29 18:15:08 +0900

    add top app bar

--- app/src/main/java/jp/shirataki707/superheroes/MainActivity.kt
+++ app/src/main/java/jp/shirataki707/superheroes/MainActivity.kt
@@ -6,20 +6,37 @@ import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.CenterAlignedTopAppBar
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import jp.shirataki707.superheroes.ui.theme.SuperheroesTheme
 
 class MainActivity : ComponentActivity() {
+    @OptIn(ExperimentalMaterial3Api::class)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
             SuperheroesTheme {
-                Scaffold { innerPadding ->
+                Scaffold(
+                    topBar = {
+                        CenterAlignedTopAppBar(
+                            title = {
+                                Text(
+                                    text = stringResource(R.string.app_name),
+                                    style = MaterialTheme.typography.displayLarge
+                                )
+                            }
+                        )
+                    }
+                ) { innerPadding ->
                     HeroesScreen(modifier = Modifier.padding(innerPadding))
                 }
             }

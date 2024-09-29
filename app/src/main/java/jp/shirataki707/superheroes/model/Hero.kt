db90eb76f6cd0b1247f8fc4e3516d8bef4e6fa85
shogo.takasaki <stakasaki707@gmail.com>
2024-09-29 17:24:29 +0900

    implemented Hero data class

---
+++ app/src/main/java/jp/shirataki707/superheroes/model/Hero.kt
@@ -0,0 +1,10 @@
+package jp.shirataki707.superheroes.model
+
+import androidx.annotation.DrawableRes
+import androidx.annotation.StringRes
+
+data class Hero(
+    @StringRes val nameRes: Int,
+    @StringRes val descriptionRes: Int,
+    @DrawableRes val imageRes: Int
+)

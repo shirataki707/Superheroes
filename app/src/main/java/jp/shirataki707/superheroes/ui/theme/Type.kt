02338d403a48b0ad4ed0c0c83bbdca5be6eaed86
shogo.takasaki <stakasaki707@gmail.com>
2024-09-29 17:21:40 +0900

    set cabin font to Type.kt

--- app/src/main/java/jp/shirataki707/superheroes/ui/theme/Type.kt
+++ app/src/main/java/jp/shirataki707/superheroes/ui/theme/Type.kt
@@ -2,33 +2,38 @@ package jp.shirataki707.superheroes.ui.theme
 
 import androidx.compose.material3.Typography
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
+import jp.shirataki707.superheroes.R
+
+val Cabin = FontFamily(
+    Font(R.font.cabin_condensed_regular),
+    Font(R.font.cabin_condensed_bold, FontWeight.Bold)
+)
 
 // Set of Material typography styles to start with
 val Typography = Typography(
     bodyLarge = TextStyle(
-        fontFamily = FontFamily.Default,
+        fontFamily = Cabin,
         fontWeight = FontWeight.Normal,
         fontSize = 16.sp,
         lineHeight = 24.sp,
         letterSpacing = 0.5.sp
-    )
-    /* Other default text styles to override
+    ),
     titleLarge = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Normal,
+        fontFamily = Cabin,
+        fontWeight = FontWeight.Bold,
         fontSize = 22.sp,
         lineHeight = 28.sp,
         letterSpacing = 0.sp
     ),
     labelSmall = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Medium,
+        fontFamily = Cabin,
+        fontWeight = FontWeight.Bold,
         fontSize = 11.sp,
         lineHeight = 16.sp,
         letterSpacing = 0.5.sp
     )
-    */
 )
